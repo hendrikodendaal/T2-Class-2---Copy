570e35b120963e482b8b7997af7575e2b5bbc706
hendrikodendaal <221140@virtualwindow.co.za>
2024-04-30 12:54:43 +0200

    Class week 2

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+console.log("I am" + 77 + "years old")
+console.log(37 + 24)
+
+let ticketNum = 3;
+const pi= 3.14;
+
+let firstName = "Hendrik", surname = "Odendaal", faveNum = 7
